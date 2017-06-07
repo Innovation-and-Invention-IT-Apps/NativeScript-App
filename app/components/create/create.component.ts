42a7f37dfc96c94a56ed0c915b29190769fb0b7b
niranjan-reddy <niranjan-reddy@gmx.com>
2017-06-07 18:46:49 +0530

    Final Code

---
+++ app/components/create/create.component.ts
@@ -0,0 +1,33 @@
+import { Component } from "@angular/core";
+import { Location } from "@angular/common";
+import * as ApplicationSettings from "application-settings";
+
+
+@Component({
+  selector: "list",
+  templateUrl: "./components/create/create.xml"
+})
+
+export class CreateComponent {
+
+  private location: Location;
+  public personList: Array<Object>;
+  public firstname: string;
+  public lastname: string;
+
+  constructor(location: Location) {
+    this.location = location;
+    this.personList = JSON.parse(ApplicationSettings.getString("people", "[]"));
+    this.firstname = "";
+    this.lastname = "";
+  }
+
+  save() {
+    if(this.firstname != "" && this.lastname != "") {
+      this.personList.push({firstname: this.firstname, lastname: this.lastname});
+      ApplicationSettings.setString("people", JSON.stringify(this.personList));
+      this.location.back();
+    }
+  }
+
+}
